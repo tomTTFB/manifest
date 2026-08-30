8c2fac283ae0254c1a709dd79706852761de4fca
Tomas Wiles <tomaswiles@gmail.com>
2026-08-30 21:47:36 +0100

    Add monitor top bar with title and clock

---
+++ manifest.lua
@@ -0,0 +1,35 @@
+local monitor = peripheral.find("monitoritor")
+if not monitor then
+    print("No monitoritor attached.")
+    return
+end
+
+monitor.setTextScale(1)
+local w = monitor.getSize()
+
+monitor.setBackgroundColour(colours.black)
+monitor.clear()
+
+local function draw_bar(time)
+    monitor.setBackgroundColour(colours.lightGrey)
+    monitor.setTextColour(colours.black)
+
+    monitor.setCursorPos(1, 1)
+    monitor.write(string.rep(" ", w))
+
+    monitor.setCursorPos(2, 1)
+    monitor.write("Manifest")
+
+    monitor.setCursorPos(w - #time, 1)
+    monitor.write(time)
+end
+
+local last
+while true do
+    local now = textutils.formatTime(os.time(), true)
+    if now ~= last then
+        draw_bar(now)
+        last = now
+    end
+    sleep(0.5)
+end
